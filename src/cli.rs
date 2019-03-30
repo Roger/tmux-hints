ed7630d348c02e828ad1666243c1b9ee3bf3d7ea
roger <rogerduran@gmail.com>
2019-03-30 20:48:24 +0100

    use clap for cli
    this add a better cli with help and error handling

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use clap::{App, SubCommand, ArgMatches};
+
+
+pub fn args<'a>() -> ArgMatches<'a> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("inner").about("Internal command to run inside a new tmux window"),
+        ).subcommand(
+            SubCommand::with_name("config").about("Print current configuration")
+        )
+        .get_matches()
+}
